acc003813a6e23db1946e878a55f3ce2bfac91ed
Gabriel Hazi <hazigabriel@gmail.com>
2020-07-16 18:34:52 +0300

    began adding js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+ const changePage = function(){ 
+ 	const toHome = function(){
+ 		document.querySelector(".home-screen").style.display = "block";
+ 		document.querySelector(".game-screen").style.display = "none";
+ 	};
+ 	const toGameScreen = function(){
+ 		document.querySelector(".home-screen").style.display = "none";
+ 		document.querySelector(".game-screen").style.display = "block";
+ 	};
+ 	return {
+ 		toHome,
+ 		toGameScreen
+ 	}
+ }
+const getUserInput = function(){
  
+
+
+
+
+}
