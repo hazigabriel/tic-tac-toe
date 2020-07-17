3d0a9ffd14d26b43074dbfe4dccf319392f46fc1
Gabriel Hazi <hazigabriel@gmail.com>
2020-07-17 18:15:52 +0300

    added getUserInput, changePage and resetField modals

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- const changePage = function(){ 
+ const changePageModal = (function(){ 
  	const toHome = function(){
  		document.querySelector(".home-screen").style.display = "block";
  		document.querySelector(".game-screen").style.display = "none";
@@ -11,11 +11,58 @@
  		toHome,
  		toGameScreen
  	}
- }
+})();
+
+const changePage = (function(){
+	let submitButton = document.querySelector(".submitPlayerName");
+	let homeScreenButton = document.querySelector(".homeScreenButton");
+
+	submitButton.addEventListener("click", function(){
+
+		if(getUserInput().player1 == "" || getUserInput().player2 == ""){
+			alert("Please chose the player names");
+		 
+		} else {
+			changePageModal.toGameScreen();
+
+		}
+
+	});
+	homeScreenButton.addEventListener("click", function(){
+		let goBack = confirm("Are you sure that you want to go back to the homepage, and reset the score?");
+		if(goBack == true){
+			changePageModal.toHome()
+			resetField.userInput()
+		}
+	})
+
+
+
+})();
 const getUserInput = function(){
- 
+	let player1 = document.querySelector(".PlayerOneName");
+	let player2 = document.querySelector(".PlayerTwoName");
+	let AiCheckbox = document.querySelector(".playAgainstAiCheckbox");
+
+	player1 = player1.value;
+	player2 = player2.value;
+	AiCheckbox = AiCheckbox.checked;
 
 
+	return {
+		player1,
+		player2,
+		AiCheckbox
+	}
 
-
-}
+};
+const resetField = (function(){
+	let userInput = function(){
+		document.querySelector(".PlayerOneName").value = "";
+		document.querySelector(".PlayerTwoName").value = "";
+		document.querySelector(".playAgainstAiCheckbox").checked = false;
+	}
+	return {
+		userInput,
+	}
+})()
